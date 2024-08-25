773a5b35d26fba1d2e3ad31fc52749efc0d197f4
Tewxzy <klebersomoliveira90@gmail.com>
2024-08-25 12:27:16 -0300

    pause-button

--- script.js
+++ script.js
@@ -7,41 +7,47 @@ let segundos = 0;
 let minutos = 0;
 let horas = 0;
 
+let intervalId;
+
 function start() {
-    setInterval(() => {
+    intervalId = setInterval(() => {
         milessimo += 10;
 
         if (milessimo === 1000) {
             milessimo = 0;
             segundos++;
-            if(segundos < 10){
-                s.innerHTML = "0" + segundos
-            }
-            if(segundos > 10){
-                s.innerHTML = segundos
+
+            if (segundos < 10) {
+                s.innerHTML = "0" + segundos;
+            } else {
+                s.innerHTML = segundos;
             }
         }
 
         if (segundos === 59) {
             segundos = 0;
             minutos++;
-            if(minutos < 10){
-                m.innerHTML = "0" + minutos
-            }
-            if(minutos > 10){
-                m.innerHTML = minutos
+
+            if (minutos < 10) {
+                m.innerHTML = "0" + minutos;
+            } else {
+                m.innerHTML = minutos;
             }
         }
 
         if (minutos === 59) {
             minutos = 0;
             horas++;
-            if(segundos < 10){
-                h.innerHTML = "0" + horas
-            }
-            if(segundos > 10){
-                h.innerHTML = horas
+
+            if (horas < 10) {
+                h.innerHTML = "0" + horas;
+            } else {
+                h.innerHTML = horas;
             }
         }
     }, 10);
+}
+
+function pause(){
+    clearInterval(intervalId);
 }
